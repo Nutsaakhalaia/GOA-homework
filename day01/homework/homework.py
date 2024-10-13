bc741add3852e8c0ef3d3ce6a1b9467268daefc5
Nutsaakhalaia <Akhalaianutsa8@gmail.com>
2024-10-13 14:01:24 +0400

    first commit

---
+++ day01/homework/homework.py
@@ -0,0 +1,24 @@
+name= "ნუცა"
+#name არის ცვლადი
+= არის ცვლადისთვის მიმნიჭებელი სიმბოლო
+"ნუცა" არის ცვლადისთვის მნიშვნელობა
+surname= "ახალაია"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name= "ნუცა" # ეს არის str(string) ტიპის ცვლადი
+age = 16 #ეს არის int( integer ) მთელი რიცხვი
+height = 169.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True #true or false
+is_ugly = False #snakecase( უბრალოდ წერის სტილი სტანდარტულად )
+
+is_ugly =False # ჯავასკრიპტული camelcase
+
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები 
+##print(name + age)
