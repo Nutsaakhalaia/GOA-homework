7d18a1dec820bbc6e991e7208caee054ed18cf1a
Nutsaakhalaia <Akhalaianutsa8@gmail.com>
2024-11-27 21:03:20 +0400

    eighth commit

---
+++ day08/homework.py/homework2.py
@@ -0,0 +1,6 @@
+name = input("Enter Your Name ")
+age = input("Enter Your Age ")
+last_name = input("Enter Your Last Name ")
+
+
+print("Hello My name is" + " " + name + " " + last_name +  + " " + "I'm" + " " + age + " " + "years old ")
